781cb67763f644686f8ae4411ab6ea26900b7fc4
ericgoalist <e.nagata@goalist.co.jp>
2017-05-10 17:51:33 +0900

    明日の日付追加

--- jp.co.goalist.training.second/src/jp/co/goalist/training/second/Training.java
+++ jp.co.goalist.training.second/src/jp/co/goalist/training/second/Training.java
@@ -1,18 +1,31 @@
 package jp.co.goalist.training.second;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Training {
 
 	public static void main(String args[]){
 
+//		Date today = new Date();
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+
+//		System.out.println("今日は"+sdf.format(today)+"です");
+
+
 		Date today = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
 
-		System.out.println("今日は"+sdf.format(today)+"です");
+		Calendar tmr = Calendar.getInstance();
+		tmr.setTime(today);
+		tmr.add(Calendar.DAY_OF_MONTH,1);
+		String tmrStr =sdf.format(tmr.getTime());
 
 
-}
+
+		System.out.println("明日は"+tmrStr+"です");
+
+	}
 
 }
