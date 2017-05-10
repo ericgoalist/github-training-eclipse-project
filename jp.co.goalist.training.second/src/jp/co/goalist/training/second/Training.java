8f0d667a1c6f88f3b316f94f96e35d329c918482
ericgoalist <e.nagata@goalist.co.jp>
2017-05-10 17:49:14 +0900

    新規と今日の日付

---
+++ jp.co.goalist.training.second/src/jp/co/goalist/training/second/Training.java
@@ -0,0 +1,18 @@
+package jp.co.goalist.training.second;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Training {
+
+	public static void main(String args[]){
+
+		Date today = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+
+		System.out.println("今日は"+sdf.format(today)+"です");
+
+
+}
+
+}
